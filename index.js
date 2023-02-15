3a7fce47989748039667de9e2875b26169818395
Frank Jover <frank@macbook-pro-de-frank.home>
2023-02-15 16:56:17 +0100

    again route

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const charactersRoute = require("./Routes/characters");
+const charactersRoute = require("./routes/characters");
+const comicsRoute = require("./routes/comics");
 
 app.use(express.json());
 app.use(cors());
 app.use(charactersRoute);
+app.use(comicsRoute);
 
 app.get("/", (req, res) => {
   res.json("WELCOME HERE !");
